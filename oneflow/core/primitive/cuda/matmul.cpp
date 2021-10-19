9b904e7dfedf69e427cf470c9335b08f59fc9351
liujuncheng <liujuncheng1022@gmail.com>
2021-10-19 14:45:33 +0800

    cuda matmul

--- oneflow/core/primitive/cuda/matmul.cpp
+++ oneflow/core/primitive/cuda/matmul.cpp
@@ -21,6 +21,8 @@ limitations under the License.
 #include "oneflow/core/primitive/include/broadcast_matmul.h"
 #include "oneflow/core/common/optional.h"
 #include "oneflow/core/device/cuda_util.h"
+#include "oneflow/core/stream/cuda_stream_context.h"
+#include "oneflow/core/common/nd_index_offset_helper.h"
 #include <cuda.h>
 
 namespace oneflow {
@@ -29,7 +31,9 @@ namespace primitive {
 
 namespace {
 
-Optional<cudaDataType_t> GetCudaDataType(DataType data_type) {
+constexpr size_t kMaxNumDims = 8;
+
+Optional<cudaDataType_t> OptCudaDataType(DataType data_type) {
   switch (data_type) {
     case kFloat: return CUDA_R_32F;
     case kDouble: return CUDA_R_64F;
@@ -41,7 +45,30 @@ Optional<cudaDataType_t> GetCudaDataType(DataType data_type) {
   }
 }
 
-Optional<cublasComputeType_t> GetComputeType(DataType data_type) {
+cudaDataType_t GetCudaDataType(DataType data_type) {
+  auto cuda_data_type = OptCudaDataType(data_type);
+  CHECK(cuda_data_type.has_value());
+  return cuda_data_type.value_or(CUDA_R_32F);
+}
+
+union CublasScalarParameter {
+  double d;
+  float s;
+};
+
+CublasScalarParameter GetCublasScalarParameter(Scalar scalar, cublasComputeType_t compute_type) {
+  CublasScalarParameter sp{};
+  if (compute_type == CUBLAS_COMPUTE_64F) {
+    sp.d = scalar.Value<double>();
+  } else if (compute_type == CUBLAS_COMPUTE_32F) {
+    sp.s = scalar.Value<float>();
+  } else {
+    UNIMPLEMENTED();
+  }
+  return sp;
+}
+
+cublasComputeType_t GetComputeType(DataType data_type) {
   switch (data_type) {
     case kFloat: return CUBLAS_COMPUTE_32F;
     case kDouble: return CUBLAS_COMPUTE_64F;
@@ -49,79 +76,129 @@ Optional<cublasComputeType_t> GetComputeType(DataType data_type) {
 #if CUDA_VERSION >= 11000
     case kBFloat16: return CUBLAS_COMPUTE_32F;
 #endif  // CUDA_VERSION >= 11000
-    default: return NullOpt;
+    default: UNIMPLEMENTED(); return CUBLAS_COMPUTE_32F;
   }
 }
 
-void GetCublasGemmExArgs(BlasTransposeType transpose_a, BlasTransposeType transpose_b,
-                         int64_t num_a_batches, int64_t num_b_batches, int64_t m, int64_t n,
-                         int64_t k, const void* a, const void* b, cublasOperation_t* cublas_trans_a,
-                         cublasOperation_t* cublas_trans_b, int* cublas_m, int* cublas_n,
-                         int* cublas_k, const void** cublas_a, int* cublas_lda,
-                         long long int* cublas_stride_a, const void** cublas_b, int* cublas_ldb,
-                         long long int* cublas_stride_b, int* cublas_ldc,
-                         long long int* cublas_stride_c) {
-  CHECK(num_a_batches == 1 || num_b_batches == 1 || num_a_batches == num_b_batches);
-  const auto ToCublasOperation = [](BlasTransposeType transpose_type,
-                                    cublasOperation_t* cublas_trans) {
+void LaunchCublasBroadcastMatmul(StreamContext* stream_ctx, DataType data_type,
+                                 BlasTransposeType transpose_a, BlasTransposeType transpose_b,
+                                 int64_t num_batch_dims, const int64_t* a_batch_dims,
+                                 const int64_t* b_batch_dims, int64_t m, int64_t n, int64_t k,
+                                 Scalar alpha, const void* a, const void* b, Scalar beta, void* c) {
+  auto* cuda_stream_ctx = CHECK_NOTNULL(dynamic_cast<CudaStreamContext*>(stream_ctx));
+  const auto cuda_data_type = GetCudaDataType(data_type);
+  const auto cublas_compute_type = GetComputeType(data_type);
+  const auto sp_alpha = GetCublasScalarParameter(alpha, cublas_compute_type);
+  const auto sp_beta = GetCublasScalarParameter(beta, cublas_compute_type);
+  const auto GetCublasOperation = [](BlasTransposeType transpose_type) {
     if (transpose_type == BlasTransposeType::N) {
-      *cublas_trans = CUBLAS_OP_T;
+      return CUBLAS_OP_T;
     } else if (transpose_type == BlasTransposeType::T) {
-      *cublas_trans = CUBLAS_OP_N;
+      return CUBLAS_OP_N;
     } else {
       UNIMPLEMENTED();
+      return CUBLAS_OP_N;
     }
   };
-  if (cublas_trans_a != nullptr) { ToCublasOperation(transpose_b, cublas_trans_a); }
-  if (cublas_trans_b != nullptr) { ToCublasOperation(transpose_a, cublas_trans_b); }
-  if (cublas_m != nullptr) { *cublas_m = n; }
-  if (cublas_n != nullptr) { *cublas_n = m; }
-  if (cublas_k != nullptr) { *cublas_k = k; }
-  if (cublas_a != nullptr) { *cublas_a = b; }
-  if (cublas_lda != nullptr) {
-    if (transpose_b == BlasTransposeType::N) {
-      *cublas_lda = n;
-    } else if (transpose_b == BlasTransposeType::T) {
-      *cublas_lda = k;
-    } else {
-      UNIMPLEMENTED();
-    }
+  const cublasOperation_t cublas_trans_a = GetCublasOperation(transpose_b);
+  const cublasOperation_t cublas_trans_b = GetCublasOperation(transpose_a);
+  const int cublas_m = n;
+  const int cublas_n = m;
+  const int cublas_k = k;
+  int cublas_lda = 0;
+  if (transpose_b == BlasTransposeType::N) {
+    cublas_lda = n;
+  } else if (transpose_b == BlasTransposeType::T) {
+    cublas_lda = k;
+  } else {
+    UNIMPLEMENTED();
   }
-  if (cublas_stride_a != nullptr) {
-    if (num_b_batches == 1) {
-      *cublas_stride_a = 0;
-    } else {
-      *cublas_stride_a = n * k;
-    }
+  int cublas_ldb = 0;
+  if (transpose_a == BlasTransposeType::N) {
+    cublas_ldb = k;
+  } else if (transpose_a == BlasTransposeType::T) {
+    cublas_ldb = m;
+  } else {
+    UNIMPLEMENTED();
   }
-  if (cublas_b != nullptr) { *cublas_b = a; }
-  if (cublas_ldb != nullptr) {
-    if (transpose_a == BlasTransposeType::N) {
-      *cublas_ldb = k;
-    } else if (transpose_a == BlasTransposeType::T) {
-      *cublas_ldb = m;
-    } else {
-      UNIMPLEMENTED();
+  const int cublas_ldc = n;
+  if (num_batch_dims == 0) {
+    const void* cublas_a = b;
+    const void* cublas_b = a;
+    void* cublas_c = c;
+    OF_CUBLAS_CHECK(cublasGemmEx(cuda_stream_ctx->cublas_handle(), cublas_trans_a, cublas_trans_b,
+                                 cublas_m, cublas_n, cublas_k, &sp_alpha, cublas_a, cuda_data_type,
+                                 cublas_lda, cublas_b, cuda_data_type, cublas_ldb, &sp_beta,
+                                 cublas_c, cuda_data_type, cublas_ldc, cublas_compute_type,
+                                 CUBLAS_GEMM_DEFAULT));
+  } else if (num_batch_dims == 1) {
+    const void* cublas_a = b;
+    const void* cublas_b = a;
+    void* cublas_c = c;
+    const int64_t a_batch_count = a_batch_dims[0];
+    const int64_t b_batch_count = b_batch_dims[0];
+    CHECK(a_batch_count == 1 || b_batch_count == 1 || a_batch_count == b_batch_count);
+    CHECK_GT(a_batch_count, 0);
+    CHECK_GT(b_batch_count, 0);
+    const int batch_count = std::max(a_batch_count, b_batch_count);
+    const long long int cublas_stride_a = b_batch_count == 1 ? 0 : cublas_m * cublas_k;
+    const long long int cublas_stride_b = a_batch_count == 1 ? 0 : cublas_k * cublas_n;
+    const long long int cublas_stride_c = cublas_m * cublas_n;
+    OF_CUBLAS_CHECK(cublasGemmStridedBatchedEx(
+        cuda_stream_ctx->cublas_handle(), cublas_trans_a, cublas_trans_b, cublas_m, cublas_n,
+        cublas_k, &sp_alpha, cublas_a, cuda_data_type, cublas_lda, cublas_stride_a, cublas_b,
+        cuda_data_type, cublas_ldb, cublas_stride_b, &sp_beta, cublas_c, cuda_data_type, cublas_ldc,
+        cublas_stride_c, batch_count, cublas_compute_type, CUBLAS_GEMM_DEFAULT));
+  } else {
+    const int64_t stride_a = m * k;
+    const int64_t stride_b = k * n;
+    const int64_t stride_c = m * n;
+    const size_t size_of_data_type = GetSizeOfDataType(data_type);
+    int64_t c_batch_count = 1;
+    int64_t c_batch_dims[kMaxNumDims];
+    for (int64_t i = 0; i < num_batch_dims; ++i) {
+      const int64_t a_batch_dim = a_batch_dims[i];
+      const int64_t b_batch_dim = b_batch_dims[i];
+      CHECK(a_batch_dim == 1 || b_batch_dim == 1 || a_batch_dim == b_batch_dim);
+      c_batch_dims[i] = std::max(a_batch_dim, b_batch_dim);
+      c_batch_count *= c_batch_dims[i];
     }
-  }
-  if (cublas_stride_b != nullptr) {
-    if (num_a_batches == 1) {
-      *cublas_stride_b = 0;
-    } else {
-      *cublas_stride_b = k * m;
-    }
-  }
-  if (cublas_ldc != nullptr) { *cublas_ldc = n; }
-  if (cublas_stride_c != nullptr) {
-    if (num_a_batches == 1 && num_b_batches == 1) {
-      *cublas_stride_c = 0;
-    } else {
-      *cublas_stride_c = n * m;
+    NdIndexOffsetHelper<int64_t, kMaxNumDims> c_index_helper(c_batch_dims, num_batch_dims);
+    NdIndexOffsetHelper<int64_t, kMaxNumDims> a_index_helper(a_batch_dims, num_batch_dims);
+    NdIndexOffsetHelper<int64_t, kMaxNumDims> b_index_helper(b_batch_dims, num_batch_dims);
+    int64_t c_batch_index[kMaxNumDims];
+    int64_t a_batch_index[kMaxNumDims];
+    int64_t b_batch_index[kMaxNumDims];
+    for (int64_t c_batch_offset = 0; c_batch_offset < c_batch_count; ++c_batch_offset) {
+      c_index_helper.OffsetToNdIndex(c_batch_offset, c_batch_index);
+      for (int64_t i = 0; i < num_batch_dims; ++i) {
+        if (a_batch_dims[i] == 1) {
+          a_batch_index[i] = 0;
+        } else {
+          a_batch_index[i] = c_batch_dims[i];
+        }
+        if (b_batch_dims[i] == 1) {
+          b_batch_index[i] = 0;
+        } else {
+          b_batch_index[i] = c_batch_dims[i];
+        }
+      }
+      const int64_t a_batch_offset = a_index_helper.NdIndexToOffset(a_batch_index);
+      const int64_t b_batch_offset = b_index_helper.NdIndexToOffset(b_batch_index);
+      const void* cublas_a =
+          static_cast<const unsigned char*>(b) + b_batch_offset * stride_b * size_of_data_type;
+      const void* cublas_b =
+          static_cast<const unsigned char*>(a) + a_batch_offset * stride_a * size_of_data_type;
+      void* cublas_c =
+          static_cast<unsigned char*>(c) + c_batch_offset * stride_c * size_of_data_type;
+      OF_CUBLAS_CHECK(cublasGemmEx(cuda_stream_ctx->cublas_handle(), cublas_trans_a, cublas_trans_b,
+                                   cublas_m, cublas_n, cublas_k, &sp_alpha, cublas_a,
+                                   cuda_data_type, cublas_lda, cublas_b, cuda_data_type, cublas_ldb,
+                                   &sp_beta, cublas_c, cuda_data_type, cublas_ldc,
+                                   cublas_compute_type, CUBLAS_GEMM_DEFAULT));
     }
   }
 }
-
-constexpr size_t kMaxNumDims = 8;
 
 void SimplifyBroadcastMatmul(size_t num_a_dims, const int64_t* a_dims, size_t num_b_dims,
                              const int64_t* b_dims, BlasTransposeType transpose_a,
@@ -181,21 +258,9 @@ void SimplifyBroadcastMatmul(size_t num_a_dims, const int64_t* a_dims, size_t nu
   }
 }
 
-void DoMatmul(StreamContext* stream_ctx, DataType data_type, int64_t m, int64_t n, int64_t k,
-              Scalar alpha, const void* a, const void* b, Scalar beta, void* c) {}
-
-void DoBatchMatmul(StreamContext* stream_ctx, DataType data_type, int64_t num_a_batches,
-                   int64_t num_b_batches, int64_t m, int64_t n, int64_t k, Scalar alpha,
-                   const void* a, const void* b, Scalar beta, void* c) {}
-
-void DoBroadcastMatmul(StreamContext* stream_ctx, DataType data_type, int64_t num_batch_dims,
-                       const int64_t* a_batch_dims, const int64_t* b_batch_dims, int64_t m,
-                       int64_t n, int64_t k, Scalar alpha, const void* a, const void* b,
-                       Scalar beta, void* c) {}
-
 }  // namespace
 
-class BroadcastMatmulImpl : public BroadcastMatmul {
+class BroadcastMatmulImpl : public Matmul, public BatchMatmul, public BroadcastMatmul {
  public:
   OF_DISALLOW_COPY_AND_MOVE(BroadcastMatmulImpl);
   BroadcastMatmulImpl(DataType data_type, BlasTransposeType transpose_a,
@@ -209,6 +274,40 @@ class BroadcastMatmulImpl : public BroadcastMatmul {
   BlasTransposeType transpose_a() const override { return transpose_a_; }
   virtual BlasTransposeType transpose_b() const override { return transpose_b_; }
 
+  void Launch(StreamContext* stream_ctx, size_t m, size_t n, size_t k, Scalar alpha, const void* a,
+              const void* b, Scalar beta, void* c) override {
+    Launch(stream_ctx, 1, m, n, k, alpha, a, b, beta, c);
+  }
+
+  // BatchMatmul
+  void Launch(StreamContext* stream_ctx, size_t num_batches, size_t m, size_t n, size_t k,
+              Scalar alpha, const void* a, const void* b, Scalar beta, void* c) override {
+    int64_t a_dims[3];
+    int64_t b_dims[3];
+    a_dims[0] = num_batches;
+    b_dims[0] = num_batches;
+    if (transpose_a_ == BlasTransposeType::N) {
+      a_dims[1] = m;
+      a_dims[2] = k;
+    } else if (transpose_a_ == BlasTransposeType::T) {
+      a_dims[1] = k;
+      a_dims[2] = m;
+    } else {
+      UNIMPLEMENTED();
+    }
+    if (transpose_b_ == BlasTransposeType::N) {
+      b_dims[1] = k;
+      b_dims[2] = n;
+    } else if (transpose_b_ == BlasTransposeType::T) {
+      b_dims[1] = n;
+      b_dims[2] = k;
+    } else {
+      UNIMPLEMENTED();
+    }
+    Launch(stream_ctx, alpha, 3, a_dims, a, 3, b_dims, b, beta, c);
+  }
+
+  // BroadcastMatmul
   void Launch(StreamContext* stream_ctx, Scalar alpha, size_t num_a_dims, const int64_t* a_dims,
               const void* a, size_t num_b_dims, const int64_t* b_dims, const void* b, Scalar beta,
               void* c) override {
@@ -220,14 +319,8 @@ class BroadcastMatmulImpl : public BroadcastMatmul {
     int64_t b_batch_dims[kMaxNumDims]{};
     SimplifyBroadcastMatmul(num_a_dims, a_dims, num_b_dims, b_dims, transpose_a_, transpose_b_, &m,
                             &n, &k, &num_batch_dims, a_batch_dims, b_batch_dims);
-    if (num_batch_dims == 0) {
-      DoMatmul(stream_ctx, data_type_, m, n, k, alpha, a, b, beta, c);
-    } else if (num_batch_dims == 1) {
-      DoBatchMatmul(stream_ctx, data_type_, a_batch_dims[0], b_batch_dims[0], m, n, k, alpha, a, b,
-                    beta, c);
-    } else {
-      // DoBroadcastMatmul();
-    }
+    LaunchCublasBroadcastMatmul(stream_ctx, data_type_, transpose_a_, transpose_b_, num_batch_dims,
+                                a_batch_dims, b_batch_dims, m, n, k, alpha, a, b, beta, c);
   }
 
  private:
@@ -235,6 +328,44 @@ class BroadcastMatmulImpl : public BroadcastMatmul {
   BlasTransposeType transpose_a_;
   BlasTransposeType transpose_b_;
 };
+
+class MatmulFactoryImpl : public MatmulFactory {
+ public:
+  OF_DISALLOW_COPY_AND_MOVE(MatmulFactoryImpl);
+  MatmulFactoryImpl() = default;
+  ~MatmulFactoryImpl() override = default;
+
+  std::unique_ptr<Matmul> New(DataType data_type, BlasTransposeType transpose_a,
+                              BlasTransposeType transpose_b) override {
+    auto cuda_data_type = OptCudaDataType(data_type);
+    if (cuda_data_type.has_value()) {
+      return std::make_unique<BroadcastMatmulImpl>(data_type, transpose_a, transpose_b);
+    } else {
+      return nullptr;
+    }
+  }
+};
+
+REGISTER_PRIMITIVE_FACTORY(DeviceType::kGPU, MatmulFactory, MatmulFactoryImpl);
+
+class BatchMatmulFactoryImpl : public BatchMatmulFactory {
+ public:
+  OF_DISALLOW_COPY_AND_MOVE(BatchMatmulFactoryImpl);
+  BatchMatmulFactoryImpl() = default;
+  ~BatchMatmulFactoryImpl() override = default;
+
+  std::unique_ptr<BatchMatmul> New(DataType data_type, BlasTransposeType transpose_a,
+                                   BlasTransposeType transpose_b) override {
+    auto cuda_data_type = OptCudaDataType(data_type);
+    if (cuda_data_type.has_value()) {
+      return std::make_unique<BroadcastMatmulImpl>(data_type, transpose_a, transpose_b);
+    } else {
+      return nullptr;
+    }
+  }
+};
+
+REGISTER_PRIMITIVE_FACTORY(DeviceType::kGPU, BatchMatmulFactory, BatchMatmulFactoryImpl);
 
 class BroadcastMatmulFactoryImpl : public BroadcastMatmulFactory {
  public:
@@ -245,7 +376,7 @@ class BroadcastMatmulFactoryImpl : public BroadcastMatmulFactory {
   std::unique_ptr<BroadcastMatmul> New(DataType data_type, BlasTransposeType transpose_a,
                                        BlasTransposeType transpose_b,
                                        size_t max_num_dims) override {
-    auto cuda_data_type = GetCudaDataType(data_type);
+    auto cuda_data_type = OptCudaDataType(data_type);
     if (max_num_dims <= kMaxNumDims && cuda_data_type.has_value()) {
       return std::make_unique<BroadcastMatmulImpl>(data_type, transpose_a, transpose_b);
     } else {
@@ -253,6 +384,8 @@ class BroadcastMatmulFactoryImpl : public BroadcastMatmulFactory {
     }
   }
 };
+
+REGISTER_PRIMITIVE_FACTORY(DeviceType::kGPU, BroadcastMatmulFactory, BroadcastMatmulFactoryImpl);
 
 }  // namespace primitive
 
