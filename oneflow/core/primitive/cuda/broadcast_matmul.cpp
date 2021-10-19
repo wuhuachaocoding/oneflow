9bf77a19dfe3078a1465140e13d5f605c68fd6db
liujuncheng <liujuncheng1022@gmail.com>
2021-10-19 17:04:23 +0800

    refine

--- oneflow/core/primitive/cuda/broadcast_matmul.cpp
+++ oneflow/core/primitive/cuda/broadcast_matmul.cpp
@@ -202,64 +202,6 @@ void LaunchCublasBroadcastMatmul(StreamContext* stream_ctx, DataType data_type,
   }
 }
 
-void SimplifyBroadcastMatmul(size_t num_a_dims, const int64_t* a_dims, size_t num_b_dims,
-                             const int64_t* b_dims, BlasTransposeType transpose_a,
-                             BlasTransposeType transpose_b, int64_t* m, int64_t* n, int64_t* k,
-                             int64_t* num_batch_dims, int64_t* a_batch_dims,
-                             int64_t* b_batch_dims) {
-  CHECK_GE(num_a_dims, 2);
-  CHECK_GE(num_b_dims, 2);
-  if (transpose_a == BlasTransposeType::N) {
-    *m = a_dims[num_a_dims - 2];
-    *k = a_dims[num_a_dims - 1];
-  } else if (transpose_a == BlasTransposeType::T) {
-    *m = a_dims[num_a_dims - 1];
-    *k = a_dims[num_a_dims - 2];
-  } else {
-    UNIMPLEMENTED();
-  }
-  if (transpose_b == BlasTransposeType::N) {
-    CHECK_EQ(b_dims[num_b_dims - 2], *k);
-    *n = b_dims[num_b_dims - 1];
-  } else if (transpose_b == BlasTransposeType::T) {
-    CHECK_EQ(b_dims[num_b_dims - 1], *k);
-    *n = b_dims[num_b_dims - 2];
-  } else {
-    UNIMPLEMENTED();
-  }
-  const int64_t num_a_batch_dims = num_a_dims - 2;
-  const int64_t num_b_batch_dims = num_b_dims - 2;
-  const int64_t num_max_batch_dims = std::max(num_a_batch_dims, num_b_batch_dims);
-  const int64_t num_a_padding_dims = num_max_batch_dims - num_a_batch_dims;
-  const int64_t num_b_padding_dims = num_max_batch_dims - num_b_batch_dims;
-  *num_batch_dims = 0;
-  for (int64_t i = 0; i < num_max_batch_dims; ++i) {
-    const int64_t a_dim = i < num_a_padding_dims ? 1 : a_dims[i - num_a_padding_dims];
-    const int64_t b_dim = i < num_b_padding_dims ? 1 : b_dims[i - num_b_padding_dims];
-    if (a_dim == 1 && b_dim == 1) {
-      continue;
-    } else if (*num_batch_dims != 0
-               && ((a_dim == 1 && a_batch_dims[*num_batch_dims - 1] == 1)
-                   || (b_dim == 1 && b_batch_dims[*num_batch_dims - 1] == 1)
-                   || (a_dim == b_dim
-                       && a_batch_dims[*num_batch_dims - 1]
-                              == b_batch_dims[*num_batch_dims - 1]))) {
-      a_batch_dims[*num_batch_dims - 1] *= a_dim;
-      b_batch_dims[*num_batch_dims - 1] *= b_dim;
-    } else {
-      CHECK(a_dim == b_dim || a_dim == 1 || b_dim == 1);
-      a_batch_dims[*num_batch_dims] = a_dim;
-      b_batch_dims[*num_batch_dims] = b_dim;
-      *num_batch_dims += 1;
-    }
-  }
-  if (*num_batch_dims >= 1 && a_batch_dims[*num_batch_dims - 1] != 1
-      && b_batch_dims[*num_batch_dims - 1] == 1 && transpose_a == BlasTransposeType::N) {
-    *m *= a_batch_dims[*num_batch_dims - 1];
-    *num_batch_dims -= 1;
-  }
-}
-
 class BroadcastMatmulImpl : public BroadcastMatmul {
  public:
   OF_DISALLOW_COPY_AND_MOVE(BroadcastMatmulImpl);
